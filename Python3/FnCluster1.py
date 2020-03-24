afa1fc030fc417291c3044327fc80bb56e43d769
Silveris <DaveFonorow@gmail.com>
2020-03-23 23:25:29 -0500

    csv read and write

--- Python3/FnCluster1.py
+++ Python3/FnCluster1.py
@@ -18,15 +18,41 @@ pathInA = os.path.join("TestReso", "Input", "inputA.csv")
 # it makes your life easier by saving the data to a varible when you read it in so its easier to access when you need it
 
 dataA = []
-
+# with open(path, what you want to do) as "Temp var name"
 with open(pathInA, "r") as file:
+    # setting the reader as a variable saves time
+    # the Delimiter is what the values are separated by in the csv. default is the ,
     reader = csv.reader(file, delimiter=',')
+    # Header saves the first line with the key for how the info is saved. not all csv's have a header
+    # next(reader) is what actually saves the first line
     header = next(reader)
+    # loop through the file and save the data into our local var
+    # this saves us from having to re-read it every time we want to look at the data or do anything with it
     for line in reader:
         dataA.append(line)
-
 print(dataA)
+print("\n\n\n")
 
+# Generate some random data we can save in a csv
+# list Comprehension
+#              output          loop-iner  iterator   loop-outer
+dataB = [[random.randint(0, 100) for j in range(5)] for i in range(10)]
+for line in dataB:
+    print(line)
+
+# Checks if path is valid. if it is, it creates a txt file containing the data
+# else it gives as os error
+try:
+    #           path    w+ is write or create if not there
+    #                            newline="" removes the blank line in-between lines
+    with open(pathOutA, "w+", newline="") as dataOut:
+        writer = csv.writer(dataOut, delimiter='\t')
+        for row in dataB:
+            writer.writerow(row)
+except OSError:
+    print("Creation of txt file failed")
+else:
+    pass
 
 
 
